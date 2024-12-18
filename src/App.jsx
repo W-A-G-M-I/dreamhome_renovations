eefff18c6c6babe624304b2a6dacf6734222ca60
Josemaria Ofurum <Josemariaofurum@gmail.com>
2024-12-19 00:06:27 +0100

    initialize setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <section className="flex-center h-screen">
+      <h1 className="text-3xl font-bold">Welcome to Dream House Renovations</h1>
+    </section>
+  );
+};
+
+export default App;
