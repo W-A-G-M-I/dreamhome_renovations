462139440df9d2b33e77acb1cec618c0ffe2294c
Lai <lookalikeiboboi@gmail.com>
2024-12-21 15:11:43 +0100

    first

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 const Contact = () => {
-  return   <section>
-    <div className="left-section">  
-     <h2>Let's Discuss <span>Your Next Project</span></h2>
-    </div>
+  return  <div className="flex w-full">
+      <section className="h-screen bg-white-400 w-1/2  ">  
+        <h2 className="font-black .font-mono text-4xl my-44 mx-14">Let's Discuss <span className="block">Your Next Project</span></h2>
+      </section>
     
-    <div className="right-section">
+      <section className="w-1/2 my-44 mx-72">
+      <input type="text" className="border-2 rounded-md w-full , px-3 h-10" placeholder="First Name" />
+      </section>
       
     </div>
-  </section>;
-  
 };
 
 export default Contact;
