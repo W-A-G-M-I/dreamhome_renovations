4aaff83e6ea5472733c27cf07d2822d445773c6b
Josemaria Ofurum <Josemariaofurum@gmail.com>
2024-12-23 21:44:44 +0100

    modified pages folder

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Form from "../components/contact/form/Form";
 import Discuss from "../components/contact/discuss/Discuss";
-import './contact.css'
 
 const Contact = () => {
   return (
-    <section className="flex-box">
+    <section className="flex">
       <Discuss />
       <Form />
     </section>
