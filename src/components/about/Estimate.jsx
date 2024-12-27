f41abcef2d4532587dfae78359b8756b3b36e327
adepoju ayomide <adepojuayomide778@gmail.com>
2024-12-27 11:17:42 +0100

    Add: First commit

---
+++ src/components/about/Estimate.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Estimate() {
+    return(
+        <div className="w-full h-96 bg-gray-800  flex flex-col justify-center items-center">
+            <p className="text-2xl font-semibold mb-4 text-white">Create your dream home.</p>
+            <p className="text-2xl font-semibold mb-4 text-white">Tell us about your project today.</p>
+            <button className="px-6 py-2 bg-blue-500 text-black rounded-lg hover:bg-blue-600">Get A Free Estimate</button>
+        </div>
+    );
+}
+export default Estimate;
