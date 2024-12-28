036d3a26b4564a88b5ec2e623990ba547a97dd53
Josemaria Ofurum <Josemariaofurum@gmail.com>
2024-12-28 14:13:49 +0100

    fixed ayo's code by removing the footer component'

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import CenteredText from '../components/about/CenteredText'
-import Image from '../components/about/Image'
-import Midddletext from '../components/about/middletxt'
-import Album from '../components/about/Album'
-import Guarantee from '../components/about/Gurantee'
-import Estimate from '../components/about/Estimate'
-import Footer from '../components/about/Footer'
+import React from "react";
+import CenteredText from "../components/about/CenteredText";
+import Image from "../components/about/Image";
+import Midddletext from "../components/about/middletxt";
+import Album from "../components/about/Album";
+import Guarantee from "../components/about/Gurantee";
+import Estimate from "../components/about/Estimate";
 
 const About = () => {
   return (
     <>
-    <CenteredText />
-    <Image /><br></br><br></br>
-    <Midddletext /><br></br><br></br>
-    <Album /><br></br><br></br>
-   <Guarantee /><br></br><br></br>
-    <Estimate />
-    <Footer />
+      <CenteredText />
+      <Image />
+      <Midddletext />
+      <Album />
+      <Guarantee />
+      <Estimate />
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
